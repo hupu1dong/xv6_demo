5216b9585508debc705082bced00d6a4d32a3920
zhangguanyi <guanyi.zhang@iwellmass.com>
2022-02-15 17:08:32 +0800

    lab2-pinpong

---
+++ user/pinpong.c
@@ -0,0 +1,24 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+#define MSGSIZE 16
+
+int main() {
+	int fd[2];
+	char buf[MSGSIZE];
+	pipe(fd);
+
+	int pid = fork();
+	if(pid > 0) {
+    write(fd[1],"ping", MSGSIZE);
+    wait(NULL);
+    read(fd[0],buf,MSGSIZE);
+    printf("%d: received %s\n",getpid(),buf);
+	} else {
+      read(fd[0],buf,MSGSIZE);
+      printf("%d: received %s\n",getpid(),buf);
+      write(fd[1],"pong", MSGSIZE);
+	}
+	return 0;
+}
